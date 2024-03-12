cf7c6deed50120ef89af43d75c7d7096a91c0b61
IrjiSyahrul <irjisyahrukchannel@gmail.com>
2024-03-13 00:22:42 +0700

    menambahkan implementasi dengan state-based

--- TPMOD4/Program.cs
+++ TPMOD4/Program.cs
@@ -11,8 +11,6 @@ internal class Program
 
         public static int getKodeposKelurahan(getKodePosEnum inputkode) {
             int[] Kodekelurahan = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 }
-
-
             return Kodekelurahan[(int)inputkode];
         }
     };
@@ -27,10 +25,10 @@ internal class Program
             public State stateAwal;
             public State stateAkhir;
             public Triger triger;
-            public Transition (State stateAwal, State stateAkhir, Triger triger) {
+            public Transition(State stateAwal, State stateAkhir, Triger triger) {
                 this.stateAwal = stateAwal;
                 this.stateAkhir = stateAkhir;
-                this.triger = triger;   
+                this.triger = triger;
             }
 
         }
@@ -40,15 +38,41 @@ internal class Program
             new Transition(State.TERKUNCI, State.TERKUNCI, Triger.KUNCI_PINTU),
 
         };
-        public State nexState(State stateAwal, State stateAkhir, Triger triger)
+        public State GetState(State stateAwal, State stateAkhir, Triger triger)
         {
+             stateAkhir = stateAwal;
 
-        };
+            for (int i = 0; i < transisi.Length; i++)
+            {
+                Transition perubahan = transisi[i];
+                if (stateAwal == perubahan.stateAwal && triger == perubahan.triger)
+                {
+                    stateAkhir = perubahan.stateAkhir;
+                }
+            }
+            return stateAkhir;
+        }
+        public void TrigerAktif(Triger triger)
+        {
+            currentState = GetState(currentState, triger);
+            Console.WriteLine("Pintu Terkunci");
+            if (currentState == State.TERBUKA)
+            {
+                Console.WriteLine("Pintu tidak Terkunci");
+            }
+        }
 
+        private State GetState(State currentState, Triger triger)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     private static void Main(string[] args)
     {
-        
+        DoorMachine objKunci = new DoorMachine();
+        Console.WriteLine(objKunci.currentState);
+        objKunci.TrigerAktif(Triger.KUNCI_PINTU);
+        objKunci.TrigerAktif(Triger.BUKA_KUNCI);
     }
 }
