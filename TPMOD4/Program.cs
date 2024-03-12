8766f0458d99b5fc1a1f8563634dee2c254ed8fa
IrjiSyahrul <irjisyahrukchannel@gmail.com>
2024-03-12 18:52:50 +0700

    menambahkan implementasi dengan table-driven

--- TPMOD4/Program.cs
+++ TPMOD4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection.Metadata.Ecma335;
+
+internal class Program
+{
+    class KodePos
+    {
+        public enum getKodePosEnum
+        {
+            Batununggal,Kujangsari,Mengger,Wates,Cijaura,Jatisari,Margasari,Sekejati,Kebonwaru,Maleer,Samoja
+        }
+
+        public static int getKodeposKelurahan(getKodePosEnum inputkode) {
+            int[] Kodekelurahan = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 }
+
+
+            return Kodekelurahan[(int)inputkode];
+        }
+    };
+
+    private static void Main(string[] args)
+    {
+        
+    }
+}
